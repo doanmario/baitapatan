fbb29a3ea989fcf383ca05b04d386b42126dd899
DoanNT <nguyentiendoanhn09@gmail.com>
2020-11-09 21:56:38 +0700

    update bai dem so luong so nguyen to

---
+++ Array/125_soLuongSoNguyenToNhoHon100.js
@@ -0,0 +1,29 @@
+function kiemtrasonguyento(n) 
+{
+    if(n<2)
+    {
+        return false;
+    }
+    else
+    {
+        for(var i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+var arr=[2,3,5,7,11,13,22,25,26,27]
+var count=0;
+ for( var i=0;i<100;i++)
+    {
+            if(kiemtrasonguyento(arr[i])==true && arr[i]<100)
+            {
+                count ++;
+            }
+           // return count;
+    }
+    console.log("số lượng số nguyên tô là "+count); 
