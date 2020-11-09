9146f87e3db47232f714a199bfb3e12445d9b969
DoanNT <nguyentiendoanhn09@gmail.com>
2020-11-09 23:03:41 +0700

    update bai 133 liet ke vi tri các sô âm trong mang

---
+++ Array/133_lietkevitriam.js
@@ -0,0 +1,10 @@
+    var mangam=[-2,3,4,-5,-1,-4];
+    var vitri=[];
+    for(let i=0;i<=mangam.length;i++)
+    {
+        if(mangam[i]<0)
+        {
+            vitri.push(i);
+        }
+    }
+    console.log("Các vi trí âm là ",vitri)
