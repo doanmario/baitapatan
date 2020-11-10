8a5604ea10fcc2053894d049b3480afbebfeaca3
DoanNT <nguyentiendoanhn09@gmail.com>
2020-11-10 07:17:41 +0700

    chua xong dang fix

---
+++ Array/135_timvitriduongdautien.js
@@ -0,0 +1,21 @@
+var mangduong=[-1,-2,-3,4,5,6,7,8,9];
+var vitri;
+for(let i=0;i<=mangduong.length;i++)
+{
+    if(mangduong[i]>0)
+    {
+    return mangduong[i];
+    break;
+    }
+    
+} 
+return -1;
+
+
+
+
+
+
+ 
+
+
