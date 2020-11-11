3b931150688ddb71b5c6896634f556da6b4a4367
DoanNT <nguyentiendoanhn09@gmail.com>
2020-11-11 07:05:07 +0700

    cho fix 140

---
+++ Array/140_timgiatringuyenduongnhonhat.js
@@ -0,0 +1,15 @@
+function timgiatringuyenduong(arr)
+{
+    min=arr[0];
+    for(let i=1;i<arr.length;i++)
+    {
+        if( arr[i] >0 && arr[i] < min)
+        {
+            min=arr[i];
+        }
+        return min;
+    }
+    return -1;
+}
+
+console.log("Kết quả là ",timgiatringuyenduong([3,-2,-1,1,6,7,8]))
